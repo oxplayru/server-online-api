97de032d1f30ffa32ae4b9407a1f320f6d6e638d
Alex Mantis <bamttup@gmail.com>
2016-03-17 13:12:58 +0000

    Allow interact with api from any domains

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
 $f3 = require('./vendor/bcosca/fatfree-core/base.php');
 require './vendor/xpaw/php-source-query-class/SourceQuery/bootstrap.php';
 use xPaw\SourceQuery\SourceQuery;
