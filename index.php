4368747d09a78584e04fb4d3b1f1550b9b5e4ec6
Alex Mantis <bamttup@gmail.com>
2016-03-17 12:56:27 +0000

    Added update time to response

--- index.php
+++ index.php
@@ -53,6 +53,7 @@ $f3->route('GET /',
 			foreach($f3->get('servers') as $server ){
 				array_push($sv_list, array("ip" => $server["ip"], "port" => $server["port"], "name" => $server["name"], "players" => 0, "max_players" => 0, "number" => $server["number"], "status" => false));
 			}
+			$sv_list = array("last_update" => false, "servers" => $sv_list);
 		}
 
 		echo json_encode($sv_list, JSON_PRETTY_PRINT);
@@ -65,8 +66,23 @@ $f3->route('GET /update/@api_key',
 			$cache = Cache::instance();
 			$cache->load(true);
 	
-			$sv_list = getServers($f3->get('servers'), $f3->get('reconnect_attempts'));
+			$sv_list = array("last_update" => time() , "servers" => getServers($f3->get('servers'), $f3->get('reconnect_attempts')));
 			$cache->set('servers_list', $sv_list, $f3->get("cache_clear_time"));
+			
+	        echo json_encode(array("acknowledged" => true));
+		}else{
+			echo json_encode(array("acknowledged" => false, "error" => "Wrong API key"));
+		}
+    }
+);
+
+$f3->route('GET /clear/@api_key',
+    function($f3) {
+		if ($f3->get('PARAMS.api_key') == $f3->get('api_key')){
+			$cache = Cache::instance();
+			$cache->load(true);
+	
+			$cache->clear('servers_list');
 			
 	        echo json_encode(array("acknowledged" => true));
 		}else{
